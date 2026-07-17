1b3cf7667f68f246a09417c14dee1d1a71dc90a1
chuzouX <75435667+chuzouX@users.noreply.github.com>
2026-07-17 17:36:42 +0800

    fix: /room 和 /players 无参数时重定向到 SPA 对应 tab；/?page= 自动定位

--- res/main.js
+++ res/main.js
@@ -330,31 +330,14 @@ class WebDashboardPlugin {
         this.app.get(['/', '/index.html'], (_req, res) => {
             this.serveHtmlWithConfig(res, path_1.default.join(publicPath, 'index.html'));
         });
-        this.app.get(['/room', '/room.html'], (_req, res) => {
-            this.serveHtmlWithConfig(res, path_1.default.join(publicPath, 'room.html'));
+        this.app.get(['/room', '/room.html'], (req, res) => {
+            if (req.query.id) {
+                return this.serveHtmlWithConfig(res, path_1.default.join(publicPath, 'room.html'));
+            }
+            return res.redirect('/?page=rooms');
         });
-        this.app.get(['/players', '/players.html'], (_req, res) => {
-            let token = undefined;
-            if (req.cookies && req.cookies['access_token']) {
-                token = req.cookies['access_token'];
-            }
-            if (!token) {
-                const authHeader = req.headers['authorization'];
-                if (authHeader && authHeader.startsWith('Bearer ')) {
-                    token = authHeader.substring(7);
-                }
-            }
-            if (!token) {
-                return res.redirect('/');
-            }
-            const session = this.userSessions.get(token);
-            if (!session || Date.now() > session.expiresAt) {
-                return res.redirect('/');
-            }
-            if (!session.isAdmin && !session.isOwner) {
-                return res.redirect('/');
-            }
-            this.serveHtmlWithConfig(res, path_1.default.join(publicPath, 'players.html'));
+        this.app.get(['/players', '/players.html'], (req, res) => {
+            return res.redirect('/?page=players');
         });
         this.app.get(['/panel', '/panel.html'], (req, res) => {
             let token = undefined;
